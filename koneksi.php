d3de82d63e74a9919d099dfe5bcf54a4f5f8d34e
Ramandhika IP <ramandhika.ilham@gmail.com>
2021-08-13 23:00:47 +0700

    v1.0

--- koneksi.php
+++ koneksi.php
@@ -1,10 +1,10 @@
 <?php
-    define("HOST","localhost");
-    define("USER","root");
-    define("PASS","");
-    define("DB","sekolah");
+define("HOST", "localhost");
+define("USER", "root");
+define("PASS", "");
+define("DB", "sekolah");
 
-    $conn = mysqli_connect(HOST,USER,PASS,DB);
-    if(!$conn){
-        echo "Koneksi Database Gagal".mysqli_error();
-    }
+$conn = mysqli_connect(HOST, USER, PASS, DB);
+if (!$conn) {
+    echo "Koneksi Database Gagal" . mysqli_error();
+}
